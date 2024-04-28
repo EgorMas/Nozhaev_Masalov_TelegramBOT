01d0f9e5d91da76c721138faed5c77dbaa8568ec
EgorMas <yegor.masalov@mail.ru>
2024-04-28 19:55:25 +0500

    Commit 19:55 28.04.2024

--- WorkWithDB.py
+++ WorkWithDB.py
@@ -10,32 +10,47 @@ def date_translator(date):
         new_date = str(int(new_date) + 1200)
     return new_date
 
+
 def check_date(date):
-    pass
+    day, month = date.split('.')
+    year = '2024'  # високосный год, в котором есть 29.02
+    try:
+        birth_date = datetime.datetime(int(year), int(month), int(day)).date()
+        return True
+    except ValueError:
+        return False
 
 
-date = '11.03'  # дата рождения в формате "день.месяц"
-date = date_translator(date)
+def get_information(id_for_sign):
+    Fullname = os.path.join('data', 'Zodiac_Signs.db')
+    con = sqlite3.connect(Fullname)
+    cur = con.cursor()
+    query = f'SELECT Sign, Sign_Eng, Date, Element, Planet FROM Zodiac_Signs WHERE id=={id_for_sign}'
+    result = cur.execute(query).fetchone()
+    con.close()
+
+    return result
 
 
-Fullname = os.path.join('data', 'Zodiac_Signs.db')
-con = sqlite3.connect(Fullname)
-cur = con.cursor()
-query = f'SELECT Begin, Finish, id_date FROM For_search'
-result = cur.execute(query).fetchall()
-con.close()
+def make_id_from_date(date):
+    Fullname = os.path.join('data', 'Zodiac_Signs.db')
+    con = sqlite3.connect(Fullname)
+    cur = con.cursor()
+    query = f'SELECT Begin, Finish, id_date FROM For_search'
+    result = cur.execute(query).fetchall()
+    con.close()
 
-for cor in result:
-    num_1 = cor[0]
-    num_2 = cor[1]
-    if int(num_1) <= int(date) <= int(num_2):
-        id_for_sign = cor[2]
+    for cor in result:
+        num_1 = cor[0]
+        num_2 = cor[1]
+        if int(num_1) <= int(date) <= int(num_2):
+            id_for_sign = cor[2]
+    return id_for_sign
 
-Fullname = os.path.join('data', 'Zodiac_Signs.db')
-con = sqlite3.connect(Fullname)
-cur = con.cursor()
-query = f'SELECT Sign, Sign_Eng, Date, Element, Planet FROM Zodiac_Signs WHERE id=={id_for_sign}'
-result = cur.execute(query).fetchone()
-con.close()
 
-print(result)
+date = '27.12'  # дата рождения в формате "день.месяц"
+if check_date(date):
+    information = get_information(make_id_from_date(date_translator(date)))
+    print(information)
+else:
+    print("Несуществующая дата")
