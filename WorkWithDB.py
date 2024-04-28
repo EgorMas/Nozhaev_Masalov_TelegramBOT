098d8179e3b6881fcd54c8184d022d37e6587254
EgorMas <yegor.masalov@mail.ru>
2024-04-28 19:20:01 +0500

    Commit 19:19 28.04.2024

--- WorkWithDB.py
+++ WorkWithDB.py
@@ -2,18 +2,40 @@ import sqlite3
 import os
 import datetime
 
-date = '1227'  # дата рождения в формате "день.месяц"
-# Sign, Sighn_Eng, Element, Planet FROM Zodiac Signs
+
+def date_translator(date):
+    day, month = date.split('.')
+    new_date = month + day
+    if int(new_date) < 121:
+        new_date = str(int(new_date) + 1200)
+    return new_date
+
+def check_date(date):
+    pass
+
+
+date = '11.03'  # дата рождения в формате "день.месяц"
+date = date_translator(date)
+
 
 Fullname = os.path.join('data', 'Zodiac_Signs.db')
 con = sqlite3.connect(Fullname)
 cur = con.cursor()
-query = f'SELECT id, Begin, Finish FROM id_date'
-
+query = f'SELECT Begin, Finish, id_date FROM For_search'
 result = cur.execute(query).fetchall()
 con.close()
-for cor in result:
-    d1, m1 = cor[1].split('.')
-    d2, m2 = cor[2].split('.')
-    print(cor, m1 + d1, m2 + d2)
 
+for cor in result:
+    num_1 = cor[0]
+    num_2 = cor[1]
+    if int(num_1) <= int(date) <= int(num_2):
+        id_for_sign = cor[2]
+
+Fullname = os.path.join('data', 'Zodiac_Signs.db')
+con = sqlite3.connect(Fullname)
+cur = con.cursor()
+query = f'SELECT Sign, Sign_Eng, Date, Element, Planet FROM Zodiac_Signs WHERE id=={id_for_sign}'
+result = cur.execute(query).fetchone()
+con.close()
+
+print(result)
